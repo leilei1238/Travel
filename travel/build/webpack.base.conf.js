99f673019831474e74c7053e7efd3a3949c0555a
leilei1238 <397536369@qq.com>
2018-04-21 20:45:43 +0800

    new

--- travel/build/webpack.base.conf.js
+++ travel/build/webpack.base.conf.js
@@ -12,11 +12,11 @@ const createLintingRule = () => ({
   test: /\.(js|vue)$/,
   loader: 'eslint-loader',
   enforce: 'pre',
-  include: [resolve('src'), resolve('test')],
-  options: {
-    formatter: require('eslint-friendly-formatter'),
-    emitWarning: !config.dev.showEslintErrorsInOverlay
-  }
+  include: [resolve('src'), resolve('test')]
+  // options: {
+  //   formatter: require('eslint-friendly-formatter'),
+  //   emitWarning: !config.dev.showEslintErrorsInOverlay
+  // }
 })
 
 module.exports = {
